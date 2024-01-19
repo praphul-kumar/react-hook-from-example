1b32c3955de4da2f521d9e26ae92cd6a07f89bdf
Praphul Kumar <praphulkumar2023@gmail.com>
2024-01-19 12:45:08 +0530

    Created: basic Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<{email:string, password:string}>({
+    email: "",
+    password: ""
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    const error = {email: "", password: ""};
+    setErrors(error);
+
+    // manual Validation
+    if (!email.includes("@")) {
+      setErrors({...error, email: "Email must include @"});
+      return;
+    }
+
+    if (password.length < 8) {
+      setErrors({...error, password: "Password must atleat 8 chars long."});
+      return;
+    }
+
+    // Form Submission
+    console.log('Form Submitted');
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login Form</h2>
+        <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        {errors.email && <div className="text-red">{errors.email}</div>}
+        <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        {errors.password && <div className="text-red">{errors.password}</div>}
+
+        <button type="submit">Submit</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

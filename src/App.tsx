b0dd92dde6d8d5485966f9f587e58e7af27b8a3a
Praphul Kumar <praphulkumar2023@gmail.com>
2024-01-19 14:05:48 +0530

    Added: Form using React hook Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+
+type FormFields = {
+  email: string,
+  password: string
+}
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState<{email:string, password:string}>({
-    email: "",
-    password: ""
-  });
+  const { register, handleSubmit }  = useForm<FormFields>();
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    
-    const error = {email: "", password: ""};
-    setErrors(error);
-
-    // manual Validation
-    if (!email.includes("@")) {
-      setErrors({...error, email: "Email must include @"});
-      return;
-    }
-
-    if (password.length < 8) {
-      setErrors({...error, password: "Password must atleat 8 chars long."});
-      return;
-    }
-
-    // Form Submission
-    console.log('Form Submitted');
-  };
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  }
 
   return (
     <>
-      <form className="login-form" onSubmit={handleSubmit}>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <h2>Login Form</h2>
-        <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        {errors.email && <div className="text-red">{errors.email}</div>}
-        <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        {errors.password && <div className="text-red">{errors.password}</div>}
-
+        <input {...register("email", {
+          required: true
+        })} type="text" name="email" id="email"/>
+        <input {...register("password", {
+          required: true
+        })} type="password" name="password" id="password"/>
         <button type="submit">Submit</button>
       </form>
     </>

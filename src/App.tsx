a4e4ee1f9a202ba95d0d5bc2378db976ed0e22cb
Praphul Kumar <praphulkumar2023@gmail.com>
2024-01-19 16:06:02 +0530

    Added: Validation using Zod & React hook form Resolver

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8)
+});
 
-type FormFields = {
-  email: string,
-  password: string
-}
+type FormFields = z.infer<typeof schema>;
 
 function App() {
-  const { register, handleSubmit, setError, formState: { errors, isSubmitting } }  = useForm<FormFields>();
+  const { register, handleSubmit, setError, formState: { errors, isSubmitting } }  = useForm<FormFields>({
+    defaultValues: {
+      email: "test@gmail.com"
+    },
+    resolver: zodResolver(schema),
+  });
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
     try {
       
       await new Promise((resolve) => { setTimeout(resolve, 1000); });
-      throw new Error("Something went wrong");
 
       console.log(data);
     } catch(error) {
@@ -29,25 +36,10 @@ function App() {
     <>
       <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <h2>Login Form</h2>
-        <input {...register("email", {
-          required: "Email is required",
-          validate: (value) => {
-            if (!value.includes('@')) {
-              return "Email must contain @.";
-            }
-
-            return true;
-          },
-        })} type="text" name="email" id="email"/>
+        <input {...register("email")} type="text" name="email" id="email"/>
         {errors.email && <span className="text-red">{errors.email.message}</span>}
 
-        <input {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 8,
-            message: "Password must contain 8 letters"
-          }
-        })} type="password" name="password" id="password"/>
+        <input {...register("password")} type="password" name="password" id="password"/>
         {errors.password && <span className="text-red">{errors.password.message}</span>}
 
         <button disabled={isSubmitting} type="submit">{isSubmitting ? 'Submiting...': 'Submit'}</button>
